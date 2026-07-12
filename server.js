2f1a74bf5a0aeee54e15ab94a5d2c0be3baed24a
guptasushmita706-sketch <guptasushmita706@gmail.com>
2026-07-12 15:06:25 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -46,8 +46,7 @@ app.post("/summarize", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-module.exports = app;
-const PORT = process.env.PORT || 3000;
+
 
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
