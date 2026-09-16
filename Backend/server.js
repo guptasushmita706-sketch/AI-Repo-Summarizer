e9df055f531d1a73180b12c0b043babb80dbb3bc
guptasushmita706-sketch <guptasushmita706@gmail.com>
2026-09-16 07:40:26 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -295,7 +295,7 @@ Return only valid JSON.
 
   const completion = await groq.chat.completions.create({
 
-    model: "llama-3.1-8b-instant",
+    model: "openai/gpt-oss-20b",
 
     messages: [
       {
